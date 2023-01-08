005c871a49d8e4813e11215a540b02310776ab5f
Arun Prabhath <arunprabhath2010@gmail.com>
2023-01-08 23:31:07 +0530

    updated according to snitycheck.py

--- src/api_testing.py
+++ src/api_testing.py
@@ -9,9 +9,9 @@ WELCOME_MESSAGE = "Hello User. Thank you for visiting, Please use /infarance for
 def test_home_endpoint():
     res = client.get("/")
     assert res.status_code == 200
-    assert res.text.replace('"', "") == WELCOME_MESSAGE
+    assert res.json() == WELCOME_MESSAGE
 
-def test_inferance_endpoint_success_case():
+def test_inferance_endpoint_success_case_1():
     request_body = json.dumps({
         "age": 39,
         "workclass": "State-gov",
@@ -31,6 +31,27 @@ def test_inferance_endpoint_success_case():
     res = client.post("/inferance", data=request_body)
     assert res.status_code == 200
     assert res.json() == "<=50K"
+
+def test_inferance_endpoint_success_case_2():
+    request_body = json.dumps({
+        "age": 50,
+        "workclass": "State-gov",
+        "fnlgt": 707516,
+        "education": "Bachelors",
+        "education-num": 19,
+        "marital-status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "black",
+        "sex": "Male",
+        "capital-gain": 21704,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States"
+    })
+    res = client.post("/inferance", data=request_body)
+    assert res.status_code == 200
+    assert res.json() == ">50K"
 
 def test_inferance_validation():
     request_body = json.dumps({
@@ -55,5 +76,6 @@ def test_inferance_validation():
 
 if __name__ == "__main__":
     test_home_endpoint()
-    test_inferance_endpoint_success_case()
+    test_inferance_endpoint_success_case_1()
+    test_inferance_endpoint_success_case_2()
     test_inferance_validation()
