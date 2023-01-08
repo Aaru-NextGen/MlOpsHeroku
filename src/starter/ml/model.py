c840d5c5ba6659dd9c1701bb353d58e962e478b1
Arun Prabhath <arunprabhath2010@gmail.com>
2023-01-08 15:58:06 +0530

    completed model training

--- src/starter/ml/model.py
+++ src/starter/ml/model.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
 
 
 # Optional: implement hyperparameter tuning.
@@ -17,8 +18,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-
-    pass
+    rf_model = RandomForestClassifier()
+    rf_model.fit(X_train, y_train)
+    return rf_model
 
 
 def compute_model_metrics(y, preds):
@@ -48,7 +50,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : ramdom forest model
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -57,4 +59,4 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
--- src/starter/train_model.py
+++ src/starter/train_model.py
@@ -1,13 +1,27 @@
-# Script to train machine learning model.
+import os
+import pickle
 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# Add the necessary imports for the starter code.
+from ml import data, model
 
-# Add code to load in the data.
+CLEAN_DATA = 'census.csv'
+MODEL_NAME = 'rf_model.pkl'
+ENCODER_NAME = 'encoder.pkl'
+LB = 'lb.pkl'
+
+project_dir = os.path.dirname(__file__)
+data_dir = os.path.join(project_dir, 'data')
+model_dir = os.path.join(project_dir, 'model')
+input_data = pd.read_csv(os.path.join(data_dir, CLEAN_DATA))
+
+def pickle_obj(obj, file_name):
+    lb_path = os.path.join(MODEL_NAME, file_name)
+    pickle.dump(obj, open(lb_path, 'wb'))
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(input_data, test_size=0.20)
 
 cat_features = [
     "workclass",
@@ -19,10 +33,26 @@ cat_features = [
     "sex",
     "native-country",
 ]
-X_train, y_train, encoder, lb = process_data(
+
+X_train, y_train, encoder, lb = data.process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+pickle_obj(encoder, ENCODER_NAME)
+pickle_obj(lb, LB)
 
 # Train and save a model.
+rf_model = model.train_model(X_train, y_train)
+
+pickle_obj(rf_model, MODEL_NAME)
+
+X_test, y_test, encoder, lb = data.process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
+
+
+preds = model.inference(rf_model, X_test)
+
+print('precision: {}, recall: {}, fbeta: {}'.format(
+    *model.compute_model_metrics(y_test, preds)
+))
